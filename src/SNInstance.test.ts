83d209f5453b97174c772890ab766b14d00b4dc3
Julian Hoch <julian.hoch@hoch-software.ch>
2023-09-16 19:33:10 +0200

    added more simple calls and tests

--- src/SNInstance.test.ts
+++ src/SNInstance.test.ts
@@ -1,13 +1,51 @@
-import { SNInstance } from './SNInstance';
+jest.mock('axios');
 
-test('can instantiate SNInstance', () => {
-    const instance = new SNInstance({ hostName: 'my_instance' });
-    expect(instance).toBeDefined();
-    expect(instance.hostName).toBe('my_instance');
+import { SNInstance } from './SNInstance';
+import axios from 'axios';
+
+let instance: SNInstance;
+
+beforeAll(() => {
+    instance = new SNInstance({ hostName: 'my_instance', credentials: { username: 'my_username', password: 'my_password' } });
 });
 
-test('can get instance status', async () => {
-    const instance = new SNInstance({ hostName: 'my_instance' });
+test('can instantiate SNInstance', () => {
+    expect(instance).toBeDefined();
+    expect(instance.hostName).toBe('my_instance');
+    expect(instance.getBaseUrl()).toBe('https://my_instance.service-now.com/');
+});
+
+test('can send request', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ status: 200 });
+    const response = await instance.getRequest('api/now/table/incident');
+    expect(response).toBeDefined();
+    expect(response.status).toBe(200);
+});
+
+test('errors are thrown properly', async () => {
+    jest.spyOn(axios, 'get').mockRejectedValueOnce({ response: { status: 401 } });
+
+    try {
+        await instance.isUp();
+    } catch (error) {
+        expect(error).toEqual({ response: { status: 401 } });
+    }
+});
+
+test('can get instance status (up)', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: {} });
     const isUp = await instance.isUp();
     expect(isUp).toBe(true);
+});
+
+test('can get instance status (down)', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({});
+    const isUp = await instance.isUp();
+    expect(isUp).toBe(false);
+});
+
+test('can get record', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: { result: {} } });
+    const record = await instance.getRecord('incident', '1234');
+    expect(record).toBeDefined();
 });
--- src/SNInstance.ts
+++ src/SNInstance.ts
@@ -25,18 +25,27 @@ class SNInstance {
     }
 
     /**
-     * Checks if the instance is up.
+     * Get the base url for the instance 
+     * (e.g. https://my_instance.service-now.com/).
      * 
-     * @returns true if the instance is up, false otherwise.
+     * @returns base url for the instance
      */
-    async isUp(): Promise<boolean> {
-        // check if credentials are set
+    getBaseUrl(): string {
+        return `https://${this.hostName}.service-now.com/`;
+    }
+
+    /** 
+     * This method allows to make generic GET requests to the instance.
+     * 
+     * @param url url to make the request to
+     * @returns the response from the instance
+     */
+    async getRequest(url: string): Promise<AxiosResponse> {
         if (!this.credentials) {
             throw new Error('Credentials are not set');
         }
 
-        const base = `https://${this.hostName}.service-now.com/`;
-        const url = `api/now/timeago/absolute`;
+        const base = this.getBaseUrl();
 
         const username = this.credentials.username;
         const password = this.credentials.password;
@@ -47,27 +56,85 @@ class SNInstance {
                 headers: { 'Accept-Encoding': 'deflate' },
                 baseURL: base,
             });
-            const rData = result.data;
-            console.debug("Result data: ", rData);
-
-            if (typeof rData === 'object') {
-                return true;
-            } else {
-                return false;
-            }
+            return result;
         } catch (error) {
             if (axios.isAxiosError(error)) {
                 if (error?.response?.status) {
-                    console.warn(`Could not retrieve instance status. Error ${error?.response?.status} ${error?.response?.statusText}`);
+                    console.warn(`Could not make request. Error ${error?.response?.status} ${error?.response?.statusText}`);
                 } else {
-                    console.warn(`Could not retrieve instance status. Error ${error?.message}`);
+                    console.warn(`Could not make request. Error ${error?.message}`);
                 }
                 console.debug('Full Error:', error);
             } else {
-                console.warn(`Could not retrieve instance status. Error`, error);
+                console.warn(`Could not make request. Error`, error);
             }
+            throw error;
+        }
+    }
+
+
+    /**
+     * Checks if the instance is up.
+     * 
+     * @returns true if the instance is up, false otherwise.
+     */
+    async isUp(): Promise<boolean> {
+        if (!this.credentials) {
+            throw new Error('Credentials are not set');
+        }
+
+        const url = `api/now/timeago/absolute`;
+
+        const result = await this.getRequest(url);
+        const rData = result.data;
+        console.debug("Result data: ", rData);
+
+        if (typeof rData === 'object') {
+            return true;
+        } else {
             return false;
         }
+    }
+
+    /**
+     * Will look up a single record in servicenow based on table and id.
+     * 
+     * @param table table name too look up
+     * @param id sys_id of the record
+     * @returns record, if found
+     */
+    async getRecord(table: string, id: string) {
+        const base = this.getBaseUrl();
+        const url = `api/now/table/${table}/${id}`;
+
+        const result = await this.getRequest(url);
+        return result.data.result;
+    }
+
+    /**
+     * Will look up the schema for the given table.
+     * 
+     * @param table table name too look up
+     * @returns schema of the table
+     */
+    async getSchema(table: string) {
+        const url = `api/now/doc/table/schema/${table}`;
+
+        const result = await this.getRequest(url);
+        return result.data.result;
+    }
+
+    /**
+     * Will look up the schema for the given table.
+     * 
+     * @param table table name too look up
+     * @returns schema of the table
+     */
+    async getMetadata(table: string) {
+        const url = `api/now/ui/meta/${table}`;
+
+        const result = await this.getRequest(url);
+        return result.data.result;
     }
 }
 
